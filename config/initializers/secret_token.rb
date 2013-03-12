8b85726ad4f92a9e98a4939a6c06c30afa8bdc99
Ricardo Lopes <mail@ricardolopes.net>
2013-03-12 21:54:20 +0000

    Added Rails project with devise, jquery and twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LssUsdlEditor::Application.config.secret_token = '5c92c0aeeeea93ec6e93cdecd0366cc89edfe4e10850222d0e1bd57769c2205c4c13341746394948a185eff271110f5e8079b8c28f43c84cdc95edd25c731236'
