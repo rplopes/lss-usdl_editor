be7d889686e3d317c364368c2fb415a4369d678f
Ricardo Lopes <mail@ricardolopes.net>
2013-03-25 18:22:08 +0000

    Blueprint view now showing same-time interactions right (but still unordered columns)

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -19,17 +19,65 @@ class Interaction < ActiveRecord::Base
   def self.build_interactions_blueprint(ss_id)
     interactions = Interaction.where "service_system_id = ?", ss_id
     blueprint = Array.new(4) { Array.new }
+    puts ss_id
+    puts Interaction.where "service_system_id = ?", ss_id
+    return blueprint if interactions.blank?
 
-    interactions.each do |interaction|
+    cols = build_blueprint_cols(interactions)
+    cols = sort_blueprint_cols(cols)
+
+    cols.each do |col|
       (0..3).each do |i|
-        if interaction.interaction_type == "#{subclasses[i]}Interaction"
-          blueprint[i].append interaction
-        else
-          blueprint[i].append nil
-        end
+        blueprint[i].append col[i]
       end
     end
 
     return blueprint
   end
+
+  private
+
+  def self.build_blueprint_cols(interactions)
+    cols = []
+
+    begin
+
+      puts interactions
+      interaction = interactions.shift # remove this interaction from staged interactions
+      area_of_action = interaction.interaction_type.gsub /Interaction/, ""
+
+      # Create blueprint column and append this interaction
+      col = Array.new(4)
+      col[subclasses.index(area_of_action)] = interaction
+
+      # Append to this column the interaction marked as happening at the same time
+      if interaction.interaction_during.present?
+        during = interaction.interaction_during
+        during_aoa = during.interaction_type.gsub /Interaction/, ""
+        col[subclasses.index(during_aoa)] = during
+        interactions.delete_at(interactions.index(during)) # remove this interaction from staged interactions
+      end
+
+      # Append to this column all other interactions happening at the same time
+      interaction.interactions_during.each do |during|
+        if interactions.index(during) # only add those that haven't been added yet
+          during_aoa = during.interaction_type.gsub /Interaction/, ""
+          col[subclasses.index(during_aoa)] = during
+          puts interactions.size
+          interactions.delete_at(interactions.index(during)) # remove this interaction from staged interactions
+          puts interactions.size
+        end
+      end
+
+      cols.append col
+
+    end while interactions.present?
+
+    return cols
+  end
+
+  def self.sort_blueprint_cols(cols)
+    return cols # TODO: sort
+  end
+
 end
