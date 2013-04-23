f6daae9eb29094a86f6653d27559803f80b0bdf1
Ricardo Lopes <mail@ricardolopes.net>
2013-04-23 10:15:40 +0200

    Many bug fixes and improvements in export tool

--- app/models/semantic_worker.rb
+++ app/models/semantic_worker.rb
@@ -27,46 +27,66 @@ class SemanticWorker < ActiveRecord::Base
     data = RDF::Vocabulary.new service_system.uri
     graph = RDF::Graph.new
 
+    sids = []
+    used_entities = []
+
     # Service system
-    service_sid = add_entity data, graph, 'ServiceSystem', service_system
+    service_sid = add_entity data, graph, 'ServiceSystem', service_system, sids
 
     service_system.interactions.each do |interaction|
 
       # Interactions
       interaction_type = interaction.interaction_type.present? ? interaction.interaction_type : 'Interaction'
-      interaction_sid = add_entity data, graph, interaction_type, interaction
-      graph << [service_sid, LSS_USDL.hasInteraction, interaction_sid]
+      interaction_sid = add_entity data, graph, interaction_type, interaction, sids
+    end
+    service_system.interactions.each do |interaction|
+      graph << [data[service_sid], LSS_USDL.hasInteraction, data[interaction.sid]]
 
       # Roles
       interaction.roles.each do |role|
-        role_id = add_entity data, graph, 'Role', role
-        graph << [interaction_sid, LSS_USDL.isPerformedBy, role_id]
+        if used_entities.index(role)
+          graph << [data[interaction.sid], LSS_USDL.isPerformedBy, data[role.sid]]
+          next
+        else
+          role_id = add_entity data, graph, 'Role', role, sids
+          used_entities << role
+        end
+        graph << [data[interaction.sid], LSS_USDL.isPerformedBy, data[role_id]]
 
         # Business entity
         if role.business_entity
-          business_entity_id = data[camel_case(role.business_entity.foaf_name)]
-          graph << [business_entity_id, RDF.type, GR.BusinessEntity]
-          graph << [business_entity_id, FOAF.name, role.business_entity.foaf_name]
-          graph << [business_entity_id, FOAF.page, role.business_entity.foaf_page] if role.business_entity.foaf_page.present?
-          graph << [business_entity_id, FOAF.logo, role.business_entity.foaf_logo] if role.business_entity.foaf_logo.present?
-          graph << [business_entity_id, S.telephone, role.business_entity.s_telephone] if role.business_entity.s_telephone.present?
-          graph << [business_entity_id, S.email, role.business_entity.s_email] if role.business_entity.s_email.present?
-          graph << [business_entity_id, GR.description, role.business_entity.gr_description] if role.business_entity.gr_description.present?
+          if used_entities.index(role.business_entity)
+            graph << [data[role_id], LSS_USDL.belongsToBusinessEntity, data[role.business_entity.sid]]
+            next
+          else
+            business_entity_id = camel_case(role.business_entity.foaf_name)
+            business_entity_id = "#{business_entity_id}#{Time.now.to_i}" if sids.index(business_entity_id)
+            sids << business_entity_id
+            used_entities << role.business_entity
+          end
+          graph << [data[role_id], LSS_USDL.belongsToBusinessEntity, data[business_entity_id]]
+          graph << [data[business_entity_id], RDF.type, GR.BusinessEntity]
+          graph << [data[business_entity_id], FOAF.name, role.business_entity.foaf_name]
+          graph << [data[business_entity_id], FOAF.page, role.business_entity.foaf_page] if role.business_entity.foaf_page.present?
+          graph << [data[business_entity_id], FOAF.logo, role.business_entity.foaf_logo] if role.business_entity.foaf_logo.present?
+          graph << [data[business_entity_id], S.telephone, role.business_entity.s_telephone] if role.business_entity.s_telephone.present?
+          graph << [data[business_entity_id], S.email, role.business_entity.s_email] if role.business_entity.s_email.present?
+          graph << [data[business_entity_id], GR.description, role.business_entity.gr_description] if role.business_entity.gr_description.present?
         end
       end
 
       # Time
-      time = RDF::Node.new "#{interaction_sid}Time"
-      graph << [interaction_sid, LSS_USDL.hasTime, time]
+      time = RDF::Node.new "#{interaction.sid}Time"
+      graph << [data[interaction.sid], LSS_USDL.hasTime, time]
       graph << [time, RDF.type, LSS_USDL.Time]
       # Temporal entity
-      te_sid = data["#{camel_case(interaction.label)}Time"]
+      te_sid = "#{interaction.sid.to_s.gsub(service_system.uri, '')}Time"
       te_type = interaction.time_description.present? ? "DateTimeInterval" : "ProperInterval"
-      graph << [time, LSS_USDL.hasTemporalEntity, te_sid]
-      graph << [te_sid, RDF.type, TIME[te_type]]
+      graph << [time, LSS_USDL.hasTemporalEntity, data[te_sid]]
+      graph << [data[te_sid], RDF.type, TIME[te_type]]
       if interaction.time_description.present?
-        time_description = RDF::Node.new "#{interaction_sid}DateTimeDescription"
-        graph << [te_sid, TIME.hasDateTimeDescription, time_description]
+        time_description = RDF::Node.new "#{interaction.sid}DateTimeDescription"
+        graph << [data[te_sid], TIME.hasDateTimeDescription, time_description]
         graph << [time_description, RDF.type, TIME.DateTimeDescription]
         graph << [time_description, TIME.year, interaction.time_year] if interaction.time_year
         graph << [time_description, TIME.month, interaction.time_month] if interaction.time_month
@@ -77,8 +97,8 @@ class SemanticWorker < ActiveRecord::Base
         graph << [time_description, TIME.second, interaction.time_second] if interaction.time_second
       end
       if interaction.duration_description.present?
-        duration_description = RDF::Node.new "#{interaction_sid}DurationDescription"
-        graph << [te_sid, TIME.hasDurationDescription, duration_description]
+        duration_description = RDF::Node.new "#{interaction.sid}DurationDescription"
+        graph << [data[te_sid], TIME.hasDurationDescription, duration_description]
         graph << [duration_description, RDF.type, TIME.DurationDescription]
         graph << [duration_description, TIME.years, interaction.duration_years] if interaction.duration_years
         graph << [duration_description, TIME.months, interaction.duration_months] if interaction.duration_months
@@ -89,45 +109,74 @@ class SemanticWorker < ActiveRecord::Base
       end
       # Interactions flow
       (interaction.interactions_before | [interaction.interaction_before]).each do |i|
-        graph << [te_sid, TIME.intervalAfter, data["#{camel_case(i.label)}Time"]] if i
+        graph << [data[te_sid], TIME.intervalAfter, data["#{i.sid}Time"]] if i
       end
       (interaction.interactions_during| [interaction.interaction_during]).each do |i|
-        graph << [te_sid, TIME.intervalDuring, data["#{camel_case(i.label)}Time"]] if i
+        graph << [data[te_sid], TIME.intervalDuring, data["#{i.sid}Time"]] if i
       end
       (interaction.interactions_after | [interaction.interaction_after]).each do |i|
-        graph << [te_sid, TIME.intervalBefore, data["#{camel_case(i.label)}Time"]] if i
+        graph << [data[te_sid], TIME.intervalBefore, data["#{i.sid}Time"]] if i
       end
 
       # Goals
       interaction.goals.each do |goal|
-        goal_sid = add_entity data, graph, 'Goal', goal
-        graph << [interaction_sid, LSS_USDL.hasGoal, goal_sid]
+        if used_entities.index(goal)
+          goal_sid = goal.sid
+        else
+          goal_sid = add_entity data, graph, 'Goal', goal, sids
+          used_entities << goal
+        end
+        graph << [data[interaction.sid], LSS_USDL.hasGoal, data[goal_sid]]
       end
 
       # Processes
       interaction.processes.each do |process|
-        process_sid = add_entity data, graph, 'Process', process
-        graph << [interaction_sid, LSS_USDL.belongsToProcess, process_sid]
+        if used_entities.index(process)
+          process_sid = process.sid
+        else
+          process_sid = add_entity data, graph, 'Process', process, sids
+          used_entities << process
+        end
+        graph << [data[interaction.sid], LSS_USDL.belongsToProcess, data[process_sid]]
       end
 
       # Locations
       interaction.locations.each do |location|
-        location_sid = add_entity data, graph, 'Location', location
-        graph << [interaction_sid, LSS_USDL.hasLocation, location_sid]
-        graph << [location_sid, LSS_USDL.isLocationFrom, GN[location.gn_feature.gsub(/[^0-9]*/, '')]] if location.gn_feature.present?
+        if used_entities.index(location)
+          graph << [data[interaction.sid], LSS_USDL.hasLocation, data[location.sid]]
+          next
+        else
+          location_sid = add_entity data, graph, 'Location', location, sids
+          used_entities << location
+        end
+        graph << [data[interaction.sid], LSS_USDL.hasLocation, data[location_sid]]
+        graph << [data[location_sid], LSS_USDL.isLocationFrom, GN[location.gn_feature.gsub(/[^0-9]*/, '')]] if location.gn_feature.present?
         while location.location_id
           location = location.broader_location
-          new_location_sid = add_entity data, graph, 'Location', location
-          graph << [new_location_sid, LSS_USDL.isLocationFrom, GN[location.gn_feature.gsub(/[^0-9]*/, '')]] if location.gn_feature.present?
-          graph << [location_sid, LSS_USDL.isLocatedIn, new_location_sid]
+          if used_entities.index(location)
+            graph << [data[location_sid], LSS_USDL.isLocatedIn, data[location.sid]]
+            break
+          else
+            used_entities << location
+          end
+          new_location_sid = add_entity data, graph, 'Location', location, sids
+          graph << [data[new_location_sid], LSS_USDL.isLocationFrom, GN[location.gn_feature.gsub(/[^0-9]*/, '')]] if location.gn_feature.present?
+          graph << [data[location_sid], LSS_USDL.isLocatedIn, data[new_location_sid]]
           location_sid = new_location_sid
         end
       end
 
       # Resources
       interaction.resources.each do |resource|
+        old_resource = false
         resource_type = resource.resource_type.present? ? resource.resource_type : 'Resource'
-        resource_sid = add_entity data, graph, resource_type, resource
+        if used_entities.index(resource)
+          resource_sid = resource.sid
+          old_resource = true
+        else
+          resource_sid = add_entity data, graph, resource_type, resource, sids
+          used_entities << resource
+        end
         if interaction.received_resources.index(resource)
           resource_connection = 'receivesResource'
         elsif interaction.created_resources.index(resource)
@@ -137,13 +186,14 @@ class SemanticWorker < ActiveRecord::Base
         elsif interaction.returned_resources.index(resource)
           resource_connection = 'returnsResource'
         end
-        graph << [interaction_sid, LSS_USDL[resource_connection], resource_sid]
+        graph << [data[interaction.sid], LSS_USDL[resource_connection], data[resource_sid]]
+        next if old_resource
 
         if resource.value or resource.max_value or resource.min_value or resource.unit_of_measurement.present?
           # Price specification
           if resource_type == 'FinancialResource'
             priceSpecification = RDF::Node.new "#{resource_sid}PriceSpecification"
-            graph << [resource_sid, LSS_USDL.hasPriceSpecification, priceSpecification]
+            graph << [data[resource_sid], LSS_USDL.hasPriceSpecification, priceSpecification]
             graph << [priceSpecification, RDF.type, GR.PriceSpecification]
             graph << [priceSpecification, GR.hasCurrencyValue, resource.value_] if resource.value
             graph << [priceSpecification, GR.hasMaxCurrencyValue, resource.max_value_] if resource.max_value
@@ -152,7 +202,8 @@ class SemanticWorker < ActiveRecord::Base
           # Quantitative value
           else
             quantitativeValue = RDF::Node.new "#{resource_sid}QuantitativeValue"
-            graph << [resource_sid, LSS_USDL.hasQuantitativeValue, quantitativeValue]
+            puts "#{resource_sid}QuantitativeValue"
+            graph << [data[resource_sid], LSS_USDL.hasQuantitativeValue, quantitativeValue]
             graph << [quantitativeValue, RDF.type, GR.QuantitativeValue]
             graph << [quantitativeValue, GR.hasValue, resource.value_] if resource.value
             graph << [quantitativeValue, GR.hasMaxValue, resource.max_value_] if resource.max_value
@@ -169,11 +220,17 @@ class SemanticWorker < ActiveRecord::Base
 
   private
 
-  def self.add_entity(data, graph, type, entity)
-    sid = data[camel_case(entity.label)]
-    graph << [sid, RDF.type, LSS_USDL[type]]
-    graph << [sid, RDFS.label, entity.label]
-    graph << [sid, RDFS.comment, entity.comment] if entity.comment.present?
+  def self.add_entity(data, graph, type, entity, sids)
+    sid = camel_case(entity.label)
+    sid = "#{sid}#{Time.now.to_i}" if sids.index(sid)
+    sids << sid
+    if type != 'ServiceSystem'
+      entity.sid = sid
+      entity.save
+    end
+    graph << [data[sid], RDF.type, LSS_USDL[type]]
+    graph << [data[sid], RDFS.label, entity.label]
+    graph << [data[sid], RDFS.comment, entity.comment] if entity.comment.present?
     return sid
   end
 
